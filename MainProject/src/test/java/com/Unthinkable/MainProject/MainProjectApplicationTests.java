d4958e4c3d4aae4a0e0719b0c39818eb0a3e17bc
vatsal <vatsal.gupta@unthinkable.co>
2021-12-16 11:41:37 +0530

    Created the initial Layout of the Main Project

---
+++ MainProject/src/test/java/com/Unthinkable/MainProject/MainProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Unthinkable.MainProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
